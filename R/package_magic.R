a117e894df88f2ceed983000a150b9fcdc15095a
Bela Hausmann <75233+and3k@users.noreply.github.com>
2020-04-12 20:05:07 +0200

    updated package_magic function: added GitHub Action for R CMD check

--- R/package_magic.R
+++ R/package_magic.R
@@ -17,7 +17,17 @@ package_magic <- function(quiet = TRUE, error_on = "note") {
 #'
 #' @inheritParams package_magic
 create_package_files <- function(quiet) {
-
+  # GitHub Action for R CMD check:
+  # delete file, because changed files can’t be quietly overwritten by use_github_action()
+  github_action_check_path <- usethis::proj_path(".github", "workflows", "R-CMD-check.yaml")
+  if(file.exists(github_action_check_path)) {
+    file.remove(github_action_check_path)
+  }
+  usethis::use_github_action("check-standard.yaml", save_as = "R-CMD-check.yaml")
+  github_action_check_path %>%
+    readLines() %>%
+    stringr::str_replace("^(name:).+", "\\1 R CMD check") %>%
+    cat(file = github_action_check_path, sep = "\n")
 }
 
 #' Automatically update package files
