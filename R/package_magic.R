67c252f90209e8f22fdd6ef34d9e7eb8fc325936
Bela Hausmann <75233+and3k@users.noreply.github.com>
2020-04-18 14:34:22 +0200

    fixed all linting

--- R/package_magic.R
+++ R/package_magic.R
@@ -2,7 +2,8 @@
 #'
 #' @param quiet Should all output be suppressed?
 #' @param github_actions Should GitHub Action files be written?
-#' @param rcmdcheck_args Passed on to `args` argument in [rcmdcheck::rcmdcheck()].
+#' @param rcmdcheck_args
+#'   Passed on to `args` argument in [rcmdcheck::rcmdcheck()].
 #' @inheritParams rcmdcheck::rcmdcheck
 #' @inheritParams usethis::use_spell_check
 #' @examples
@@ -10,18 +11,37 @@
 #' package_magic()
 #' }
 #' @export
-package_magic <- function(quiet = TRUE, lang = "en-GB", github_actions = TRUE, rcmdcheck_args = "--no-manual", error_on = "note") {
+package_magic <- function(quiet = TRUE, lang = "en-GB",
+                          github_actions = TRUE,
+                          rcmdcheck_args = "--no-manual",
+                          error_on = "note") {
   # TODO: quiet is not working mostly, take a capture the output approach!
   #   see devtools::document
-  create_package_files(quiet = quiet, lang = lang, github_actions = github_actions)
-  update_package_files(quiet = quiet)
-  validate_package_files(quiet = quiet, rcmdcheck_args = rcmdcheck_args, error_on = error_on)
+
+  create_package_files(
+    quiet = quiet,
+    lang = lang,
+    github_actions = github_actions
+  )
+
+  update_package_files(
+    quiet = quiet
+  )
+
+  validate_package_files(
+    quiet = quiet,
+    rcmdcheck_args = rcmdcheck_args,
+    error_on = error_on
+  )
+
+  return(invisible())
 }
 
 #' Automatically create package files
 #'
 #' @inheritParams package_magic
-create_package_files <- function(quiet = TRUE, lang = "en-GB", github_actions = TRUE) {
+create_package_files <- function(quiet = TRUE, lang = "en-GB",
+                                 github_actions = TRUE) {
   # TODO: quiet is not working because use_testthat() has no quiet argument
   # NOTE: this will cause an R CMD check if there are no tests
   #  i.e., a test needs to added too usethis::use_test('fun')
@@ -32,14 +52,14 @@ create_package_files <- function(quiet = TRUE, lang = "en-GB", github_actions = 
   usethis::use_spell_check(lang = lang, error = TRUE)
 
   if (rlang::is_true(github_actions)) {
-    create_package_files_for_github_actions(quiet = quiet)
+    create_github_action_files(quiet = quiet)
   }
 }
 
 #' Automatically create package files for GitHub Actions
 #'
 #' @inheritParams package_magic
-create_package_files_for_github_actions <- function(quiet = TRUE) {
+create_github_action_files <- function(quiet = TRUE) {
   # GitHub Action for R CMD check:
 
   github_action_check_path <-
@@ -50,7 +70,10 @@ create_package_files_for_github_actions <- function(quiet = TRUE) {
     file.remove(github_action_check_path)
   }
   # TODO: quiet is not working because use_github_action() has no quiet arg
-  usethis::use_github_action("check-standard.yaml", save_as = "R-CMD-check.yaml")
+  usethis::use_github_action(
+    "check-standard.yaml",
+    save_as = "R-CMD-check.yaml"
+  )
   github_action_check_path %>%
     readLines() %>%
     stringr::str_replace("^(name:).+", "\\1 R CMD check") %>%
@@ -80,7 +103,11 @@ update_package_files <- function(quiet = TRUE) {
 #' This function is read-only.
 #'
 #' @inheritParams package_magic
-validate_package_files <- function(quiet = TRUE, rcmdcheck_args = "--no-manual", error_on = "note") {
+validate_package_files <- function(quiet = TRUE,
+                                   rcmdcheck_args = "--no-manual",
+                                   error_on = "note") {
+  # TODO: these commands can be run in parallel
+
   # TODO: quiet is not working: there are dots printed
   lint_result <- lintr::lint_package()
   print(lint_result)
@@ -88,7 +115,12 @@ validate_package_files <- function(quiet = TRUE, rcmdcheck_args = "--no-manual",
   if (rstudioapi::isAvailable()) {
     rstudioapi::executeCommand("checkPackage")
   } else {
+    # BUG: rcmdcheck::rcmdcheck doesn’t seem to trigger the spell check?
     # TODO: quiet is not working: the final results are still printed
-    rcmdcheck::rcmdcheck(quiet = quiet, args = rcmdcheck_args, error_on = error_on)
+    rcmdcheck::rcmdcheck(
+      quiet = quiet,
+      args = rcmdcheck_args,
+      error_on = error_on
+    )
   }
 }
